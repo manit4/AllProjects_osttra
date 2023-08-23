c5b54356438fa8db6acab4695aad97d21d7f4577
Manit <manit.manu4@gmail.com>
2023-08-23 15:27:11 +0530

    21-allprojects_last

---
+++ SpringCore/src/main/java/org/osttra/Random1.java
@@ -0,0 +1,11 @@
+package org.osttra;
+
+import org.springframework.stereotype.Service;
+
+public class Random1 {
+	
+	public Random1() {
+		System.out.println("inside Random1 constr...");
+	}
+
+}
--- SpringCore/src/main/java/org/osttra/config/AutowiringConfig.java
+++ SpringCore/src/main/java/org/osttra/config/AutowiringConfig.java
@@ -1,10 +1,19 @@
 package org.osttra.config;
 
+
+import org.osttra.Random1;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan( basePackages = "org.osttra")
 public class AutowiringConfig {
+	
+	@Bean
+	public Random1 getRandom1() {
+		System.out.println("inside getRandom() of Config class");
+		return new Random1();
+	}
 	
 }
